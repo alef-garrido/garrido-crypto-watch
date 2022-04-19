b763792d82828240298bae01baa17ea252295669
alef-garrido <oa.g11@icloud.com>
2022-04-19 18:05:44 -0500

    Import coinItem

--- src/components/CoinItem.js
+++ src/components/CoinItem.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import { PropTypes } from 'prop-types';
 
-function CoinItem(props) {
-  const { data } = props;
+function CoinItem() {
   return (
-    <li>
-      <p>{data.id}</p>
-      <p>{data.symbol}</p>
+    <li className="coin--item">
+      <p>Werk</p>
     </li>
   );
 }
-
-CoinItem.propTypes = {
-  data: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    rank: PropTypes.string.isRequired,
-    symbol: PropTypes.string.isRequired,
-  }).isRequired,
-};
 
 export default CoinItem;
--- src/components/Coins.js
+++ src/components/Coins.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import CoinItem from './CoinItem';
 
 function Home() {
-  const coins = useSelector((state) => state.coins.list);
-  const status = useSelector((state) => state.coins.isloading);
   return (
     <div className="home--container">
       <ul className="home--list-container">
-        {
-          status
-            ? 'loading'
-            : coins.map((coin) => <CoinItem key={coin.id} data={coin} />)
-        }
+        <CoinItem />
       </ul>
     </div>
   );
