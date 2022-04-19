094c4b1f8e39897cb58fb1f5b895e0b54dd4ec6c
alef-garrido <oa.g11@icloud.com>
2022-04-19 18:17:51 -0500

    Add display layout & style

--- src/components/CoinItem.js
+++ src/components/CoinItem.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
+import '../styles/coin.css';
 
-function CoinItem() {
+function CoinItem(props) {
+  const { data } = props;
+  const toNumber = (i) => i * 1;
   return (
     <li className="coin--item">
-      <p>Werk</p>
+      <p className="coin--head">{data.symbol}</p>
+      <p className="coin--preview">{data.name.toUpperCase()}</p>
+      <p className="coin--preview coin-percent">
+        {
+          (toNumber(data.changePercent24Hr)) > 0
+            ? `+${(toNumber(data.changePercent24Hr)).toFixed(1)}`
+            : `${(toNumber(data.changePercent24Hr)).toFixed(1)}`
+        }
+      </p>
+      <p className="coin--preview coin-value">{`${(toNumber(data.priceUsd)).toFixed(3)} USD`}</p>
     </li>
   );
 }
+
+CoinItem.propTypes = {
+  data: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    rank: PropTypes.string.isRequired,
+    symbol: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    priceUsd: PropTypes.string.isRequired,
+    changePercent24Hr: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default CoinItem;
