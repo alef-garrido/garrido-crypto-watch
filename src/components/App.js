fd5f976e97505f443d79a76c3500ddd6a7526d03
alef-garrido <oa.g11@icloud.com>
2022-04-18 21:33:41 -0500

    Initilaize CRA

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return(
+    <main className="app--container">
+      <div>
+        <h1>Werk!</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App;
