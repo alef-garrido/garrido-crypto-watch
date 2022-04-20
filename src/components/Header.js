ec5a60a7ffe8d9f60c1c7e1296ba2a21d108820b
alef-garrido <oa.g11@icloud.com>
2022-04-20 16:17:44 -0500

    Fix search layot

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { FaSearch } from 'react-icons/fa';
 import '../styles/header.css';
 
 function Header() {
   const assets = useSelector((state) => state.coins.list);
   return (
     <header className="header--container">
-      <form>
-        <input
-          type="text"
-          placeholder="Search your coin"
-        />
-      </form>
       <div className="header--title">
         <h1>CryptoWatcher</h1>
-        <p>
+        <p className="header--subtitle">
           {`${assets.length} listed coins.`}
         </p>
       </div>
+      <form>
+        <div className="header--search">
+          <FaSearch />
+        </div>
+        <input
+          type="text"
+          placeholder="Search"
+        />
+      </form>
     </header>
   );
 }
